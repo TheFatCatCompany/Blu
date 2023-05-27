1a0ab3d74f6b99cd906625b87e26cd6e23fe1ef0
SoulsCalibre <p0990091@Gmail.com>
2023-05-27 11:43:53 -0700

    Update Scanner.dart

--- lib/BluetoothModule/Scanner.dart
+++ lib/BluetoothModule/Scanner.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:crypto_app/widgets/chart/discovered_device_data_widget.dart';
+import 'package:flutter/material.dart';
+
+
 class BluetoothScanner{
   List<BluetoothDiscoveryResult> currentDevicesList = [];
   List<BluetoothDevice> pairedDevicesList = [];
@@ -11,6 +16,34 @@ class BluetoothScanner{
     });
     return currentDevicesList;
   }
+
+  List<Widget> getWidgets() {
+    List<Widget> widgets = [];
+
+    ThemeData theme = ThemeData(
+      primarySwatch: Colors.blue,
+      brightness: Brightness.light,
+      primaryColor: Colors.lightBlue[800],
+      primaryColorBrightness: Brightness.dark,
+      primaryColorLight: Colors.lightBlue[400],
+      primaryColorDark: Colors.lightBlue[900],
+      canvasColor: Colors.white,
+      scaffoldBackgroundColor: Colors.white,
+      bottomAppBarColor: Colors.white,
+      cardColor: Colors.white,
+      dividerColor: Colors.grey[400],
+    );
+
+    const IconData icon = IconData(2);
+
+    for (BluetoothDiscoveryResult b in devices()) {
+      widgets.add(discovered_device_data_widget(true, icon, b.device.name!, b.device.address, b.device.type.stringValue, 0.0, theme));
+    }
+
+    return widgets;
+  }
+
+
 
 //get list of paired devices
   List<BluetoothDevice> pairedDevices() {
