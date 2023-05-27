e13169321eb586e830f24fcab7840344ec1d9c62
SoulsCalibre <p0990091@Gmail.com>
2023-05-27 12:17:01 -0700

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,8 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:unicons/unicons.dart';
-
+import 'dart:async';
+import 'package:crypto_app/BluetoothModule/Scanner.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,6 +20,34 @@ class _HomePageState extends State<HomePage> {
   double balance = 66032206.10;
   double profit = 35.22;
   double profitPercent = 0.22;
+  Timer? timer;
+  BluetoothScanner scanner = BluetoothScanner();
+  List<Widget> deviceWidgets = [];
+
+
+  
+  @override
+  void initState() {
+    super.initState();
+    // Start periodic timer when widget is initialized
+    timer = Timer.periodic(const Duration(seconds: 5), (Timer t) {
+      updateValues();
+    });
+  }
+
+  @override
+  void dispose() {
+    // Cancel timer when widget is gone
+    timer?.cancel();
+    super.dispose();
+  }
+
+  void updateValues(){
+    setState(() {
+      // scan again for bluetooth devices
+      deviceWidgets = scanner.getWidgets();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,78 +103,7 @@ class _HomePageState extends State<HomePage> {
         child: Padding(
           padding: EdgeInsets.only(top: 2.h),
           child: ListView(
-            children: [
-              balancePanel(balance, profit, profitPercent, themeData),
-              actionsWidget(themeData),
-              chartHomePage(
-                true,
-                CryptoFontIcons.ETH,
-                'Ethereum',
-                'ETH',
-                'USD',
-                const [
-                  FlSpot(0, 2550.18),
-                  FlSpot(1, 2500.34),
-                  FlSpot(2, 2541.34),
-                  FlSpot(3, 2540.59),
-                  FlSpot(4, 2550.60),
-                  FlSpot(5, 2639.80),
-                  FlSpot(6, 2523.71),
-                ],
-                themeData,
-              ),
-              chartHomePage(
-                true,
-                CryptoFontIcons.BTC,
-                'Bitcoin',
-                'BTC',
-                'USD',
-                const [
-                  FlSpot(0, 40005.71),
-                  FlSpot(1, 40875.23),
-                  FlSpot(2, 40800.59),
-                  FlSpot(3, 40875.12),
-                  FlSpot(4, 41875.72),
-                  FlSpot(5, 40375.49),
-                  FlSpot(6, 40700.58),
-                ],
-                themeData,
-              ),
-              chartHomePage(
-                true,
-                CryptoFontIcons.DOGE,
-                'Dogecoin',
-                'DOGE',
-                'PLN',
-                const [
-                  FlSpot(0, 0.22),
-                  FlSpot(1, 0.24),
-                  FlSpot(2, 0.28),
-                  FlSpot(3, 0.30),
-                  FlSpot(4, 0.35),
-                  FlSpot(5, 0.52),
-                  FlSpot(6, 0.58),
-                ],
-                themeData,
-              ),
-              chartHomePage(
-                true,
-                CryptoFontIcons.LTC,
-                'Litecoin',
-                'LTC',
-                'USD',
-                const [
-                  FlSpot(0, 100.40),
-                  FlSpot(1, 102.34),
-                  FlSpot(2, 98.23),
-                  FlSpot(3, 100.23),
-                  FlSpot(4, 102.10),
-                  FlSpot(5, 103.85),
-                  FlSpot(6, 103.20),
-                ],
-                themeData,
-              ),
-            ],
+            children: deviceWidgets,
           ),
         ),
       ),
