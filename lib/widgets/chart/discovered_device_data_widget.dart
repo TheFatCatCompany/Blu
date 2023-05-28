f58d72a696fe13b77b5544e93afb0801b6709ca8
TheFatCatCompany <iansho317@gmail.com>
2023-05-27 18:47:49 -0700

    Added stuff to details page

--- lib/widgets/chart/discovered_device_data_widget.dart
+++ lib/widgets/chart/discovered_device_data_widget.dart
@@ -1,6 +1,3 @@
-import 'dart:ffi';
-
-import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
