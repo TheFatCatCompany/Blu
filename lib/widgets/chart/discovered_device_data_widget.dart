58f589127254581fc3caae521d17204f0599e06c
TheFatCatCompany <iansho317@gmail.com>
2023-05-27 19:54:38 -0700

    added ignore list (does nothing rn)

--- lib/widgets/chart/discovered_device_data_widget.dart
+++ lib/widgets/chart/discovered_device_data_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'dart:ffi';
 
+import 'package:crypto_app/BluetoothModule/Scanner.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -22,6 +23,7 @@ Padding discovered_device_data_widget(
    String deviceType,
    double signalStrength,
    ThemeData themeData,
+    BluetoothScanner scanner,
    ) {
   bool ignore = true;
   Color foreground = Colors.white;
@@ -110,7 +112,7 @@ Padding discovered_device_data_widget(
                           SizedBox(
                             width: 2.w,
                           ),
-                          IgnoreButton(),
+                          IgnoreButton(scanner: scanner, deviceID: deviceCode),
                         ],
                       ),
                     ),
